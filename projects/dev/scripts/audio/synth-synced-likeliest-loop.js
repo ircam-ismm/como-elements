75915f720f4ab757a056b5a13082f91dc053b851
b-ma <benjamin.matuszewski@gmail.com>
2024-03-04 11:53:53 +0100

    feat: introduce synced likeliest player

---
+++ projects/dev/scripts/audio/synth-synced-likeliest-loop.js
@@ -0,0 +1,37 @@
+function synthLikeliestLoop(graph, helpers, audioInNode, audioOutNode, outputFrame) {
+  // console.log(graph.session.audioBuffers);
+  const audioContext = graph.como.audioContext;
+  const syncedScheduler = helpers.syncedScheduler;
+  let currentBuffer = undefined;
+
+  const synth = new helpers.synth.SyncedBufferPlayer(syncedScheduler, audioContext);
+  synth.connect(audioOutNode);
+
+  return {
+    updateParams(updates) {
+
+    },
+    process(inputFrame) {
+      const label = inputFrame.data['ml-decoder'].likeliest;
+      // query all buffers related to the label, and pick a random buffer one
+      const filenames = graph.session.labelAudioFileTable.query(label);
+      const buffers = graph.session.labelAudioFileTable.queryBuffers(label);
+//       console.log(filenames, buffers);
+      const index = Math.floor(Math.random() * buffers.length);
+      const buffer = buffers[index];
+
+      if (currentBuffer !== buffer) {
+        currentBuffer = buffer;
+        synth.stop({ fadeOutDuration: 1 });
+
+        if (buffer) {
+          synth.start(buffer, { fadeInDuration: 0.2, loop: true });
+        }
+      }
+    },
+    destroy() {
+      synth.stop({ fadeOutDuration: 0 });
+      synth.disconnect();
+    },
+  };
+}
